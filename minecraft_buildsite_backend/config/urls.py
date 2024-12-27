7a0c2a59d4e2869a22424b3aaaca531a6035d977
Ash Omaraie <49348213+aomarai@users.noreply.github.com>
2024-12-26 23:18:06 -0600

    Testing out JWT

--- minecraft_buildsite_backend/config/urls.py
+++ minecraft_buildsite_backend/config/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from buildsite_backend.views import RegisterView, ProtectedTestView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('protected/', ProtectedTestView.as_view(), name='protected'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
