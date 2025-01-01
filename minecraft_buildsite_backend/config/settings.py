2a8fa1a4dc1dd6df345b43a056fff5b3087aae71
Ash Omaraie <mrlolz@me.com>
2025-01-01 12:58:22 -0600

    Setting default password hashing algorithm to Argon2

--- minecraft_buildsite_backend/config/settings.py
+++ minecraft_buildsite_backend/config/settings.py
@@ -101,6 +101,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
