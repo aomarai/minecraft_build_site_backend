3d95a1ed1bb835a1cf05bff5a87ede2316e8d470
Ash Omaraie <49348213+aomarai@users.noreply.github.com>
2025-01-02 20:45:05 -0600

    Adding user profiles and user password changes, comments, ratings

--- minecraft_buildsite_backend/buildsite_backend/views.py
+++ minecraft_buildsite_backend/buildsite_backend/views.py
@@ -1,8 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import status
-from .serializers import RegisterSerializer
+from rest_framework import status, generics
+
+from .models import UserProfile
+from .serializers import RegisterSerializer, UserProfileSerializer, PasswordChangeSerializer
+
 
 class RegisterView(APIView):
     def post(self, request):
@@ -17,3 +20,30 @@ class ProtectedTestView(APIView):
 
     def get(self, request):
         return Response({"message": f"Hello, {request.user.username}!"}, status=status.HTTP_200_OK)
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserProfileSerializer
+
+    def get_object(self):
+        return self.request.user.profile
+
+class PublicUserProfileView(generics.RetrieveAPIView):
+    serializer_class = UserProfileSerializer
+    queryset = UserProfile.objects.all()
+    lookup_field = 'user__username'
+
+class PasswordChangeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        serializer = PasswordChangeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = request.user
+
+        if not user.check_password(serializer.validated_data['old_password']):
+            return Response({"message": "Incorrect password."}, status=status.HTTP_400_BAD_REQUEST)
+
+        user.set_password(serializer.validated_data['new_password'])
+        user.save()
+        return Response({"message": "Password changed successfully."}, status=status.HTTP_200_OK)
